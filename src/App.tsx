71f1744b60321f527b37ccb2a2704f34e42c7488
Marat Dospolov <marat.dospolov@gmail.com>
2024-04-19 03:15:14 +0200

    symbols

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css"
+import Input from "./Input"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="p-6 mx-auto max-w-96">
+      {/* symbols taken from */}
+      {/* https://gist.github.com/Gibbs/3920259 */}
+      <Input currency="PLN" symbol="&#122;&#322;" />
+      <Input currency="USD" symbol="&#36;" />
+      <Input currency="EUR" symbol="&#8364;" />
+      <Input currency="UAH" symbol="&#8372;" />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Input.tsx
@@ -0,0 +1,29 @@
+type InputProps = {
+  currency: string
+  symbol: string
+}
+
+export default function Input({ currency, symbol }: InputProps) {
+  return (
+    <div>
+      <div className="relative mt-2 rounded-md shadow-sm">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <span className="text-gray-500 sm:text-sm">{symbol}</span>
+        </div>
+        <input
+          type="text"
+          name="price"
+          id="price"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-7 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="0.00"
+          aria-describedby="price-currency"
+        />
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <span className="text-gray-500 sm:text-sm" id="price-currency">
+            {currency}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
