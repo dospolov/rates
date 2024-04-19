15ebb623c19c46b56d29dd2c2610e3a11ec67914
Marat Dospolov <marat.dospolov@gmail.com>
2024-04-19 20:26:16 +0200

    rates provided by

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,12 @@ function App() {
             dispatch={dispatch}
           />
         ))}
-        <p className="text-slate-700">App version: 0.14</p>
+        <div className="text-slate-700 space-y-1">
+          <p>App version: 0.15</p>
+          <p>
+            Rates provided by: <a href="https://fxratesapi.com/">FXRatesAPI</a>
+          </p>
+        </div>
       </div>
     </div>
   )
